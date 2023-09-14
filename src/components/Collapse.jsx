a41ed5c34cc56f743b89a4c747269a73b5187f22
thibbns <thib.bns@outlook.com>
2023-09-14 15:05:09 +0200

    animation fleche collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,34 +1,34 @@
 import '../styles/collapse.scss'
 import { useState } from 'react'
 import arrowTop from '../assets/arrow__top.svg'
-import arrowDown from '../assets/arrow__down.svg'
 
 function Collapse({ title, description }) {
   const [isOpen, setIsOpen] = useState(false)
 
-  return isOpen ? (
+  const handleToggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
     <div className="collapse">
       <div className="collapse__title">
         <div className="collapse__title__text">{title}</div>
-        <div className="collapse__title__arrow">
-          <div onClick={() => setIsOpen(false)}>
-            <img src={arrowTop} alt="arrow top" />
+        <div
+          className={`collapse__title__arrow ${isOpen ? 'rotate-180' : ''}`}
+          onClick={handleToggle}
+        >
+          <div>
+            <img src={arrowTop} alt="arrow" />
           </div>
         </div>
       </div>
-      <div className="collapse__description">
+
+      <div
+        className={`collapse__description ${
+          isOpen ? 'collapse__description--open' : ''
+        }`}
+      >
         <div className="collapse__description__text">{description}</div>
-      </div>
-    </div>
-  ) : (
-    <div className="collapse">
-      <div className="collapse__title">
-        <div className="collapse__title__text">{title}</div>
-        <div className="collapse__title__arrow">
-          <div onClick={() => setIsOpen(true)}>
-            <img src={arrowDown} alt="arrow down" />
-          </div>
-        </div>
       </div>
     </div>
   )
